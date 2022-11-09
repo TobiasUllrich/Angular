3a716238e8f20ca065f0c23556b387fe4a1a7cb2
Tobias Ullrich <ullrich.tobias@gmx.de>
2022-11-09 21:46:29 +0100

    imprint and data-protection added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,17 @@
-import { Component, OnInit } from '@angular/core'; //OnInit kommt aus der Angular-Library
+import { Component, OnInit } from '@angular/core'; 
+import { Router } from '@angular/router'; 
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent { // Unsere Componente implementiert OnInit
   // ************************************************ JAVASCRIPT CODE HIER ********************************************************************
-  
+
+  constructor(public router: Router){}
+
 
 
 // ************************************************ JAVASCRIPT CODE HIER ********************************************************************
